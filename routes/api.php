c980e3f2aa1ee900f7fbe564be9f35b7d13fb670
Christian Roi <pockiemaster@gmail.com>
2023-05-14 23:52:49 +0800

    Added User specific update image and profile information show

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CarouselItemsController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Api\ProfileController;
 use App\Models\CarouselItems;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,6 +28,7 @@ Route::post('/user',[UserController::class, 'store'])->name('user.store');
 Route::middleware(['auth:sanctum'])->group(function () {
 Route::post('/logout',[AuthController::class, 'logout']);
 
+    // Admin APIs
     Route::controller(CarouselItemsController::class)->group(function () {
         Route::get('/carousel',             'index');
         Route::get('/carousel/{id}',        'show');
@@ -42,7 +44,12 @@ Route::post('/logout',[AuthController::class, 'logout']);
         Route::put('/user/{id}',            'update')->name('user.update');
         Route::put('/user/email/{id}',      'email')->name('user.email');
         Route::put('/user/password/{id}',   'password')->name('user.password');
+        Route::put('/user/image/{id}',      'image')->name('user.image');
     });
+
+    // User Specific APIs
+    Route::get('/profile/show',            [ProfileController::class, 'show']);
+    Route::put('/profile/image',            [ProfileController::class, 'image'])->name('profile.image');
 
 });
 
